d663425cbaabf67a3e286c20120e7ec2f404e39c
Mariayekm <m.marshalldowe@gmail.com>
2024-08-11 00:26:29 +0100

    create new data structure that implements interface

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -8,11 +8,30 @@ type ICheckout interface {
 	GetTotalPrice() (totalPrice int, err error)
 }
 
-type checkout struct {
+type myCheckout struct {
 	scannedProducts map[string]int
 	total           int
 }
 
+// Make sure myCheckout implements ICheckout
+var _ ICheckout = myCheckout{}
+
+// Create a checkout instance
+func NewCheckout() myCheckout {
+	return myCheckout{}
+}
+
+func (c myCheckout) Scan(SKU string) (err error) {
+	return err
+}
+
+func (c myCheckout) GetTotalPrice() (totalPrice int, err error) {
+	return totalPrice, err
+}
+
 func main() {
 	fmt.Println("Running checkout program")
+	newCheckout := NewCheckout()
+	total, _ := newCheckout.GetTotalPrice()
+	fmt.Println("here is total ", total)
 }
